b81127a37233031700403a070093fd8d9c8cf588
manav1999 <manav.189303123@muj.manipal.edu>
2020-03-11 15:34:00 +0530

    Hobbies and Pid removed from the DAILY_ROUTINE TABLE in Schema , insert_data updated , main.py updated

--- insert_data.py
+++ insert_data.py
@@ -1,20 +1,37 @@
 import pandas as pd
 import datetime
 
-def inputw():
-        ''' inputw() takes various inputs from user during execution and returns a dictonary '''
-        print("DATE",datetime.date.today())
-        x=int(input("Amount of sleep "))
-        y=int(input("classwork "))
-        z=int(input("self learning " ))
-        u=int(input("classes attended "))
-        if (x+y+z+u)<=24:
-                q=input('Quality of sleep ')
-                m=input('mood ') 
-                o=bool(input("alchol/pot "))
-                weekend=lambda weekend : True if (x>=5) else False
-                return dict([('d', datetime.date.today()),('W', weekend(datetime.date.today().weekday())),('g1',x),('g2',q),('g3',u),('g4',z),('g5',y),('g6',m),('g7',o)])
-        
-        else:
-                print("Incorrect data ")
-                return {}
+def daily_routine():
+	''' takes input from the user for table daily_routine '''
+	print("DATE",datetime.date.today()) #for test purposes 
+	H_S=int(input("Amount of sleep "))
+	C_A=int(input("Classes Atteneded "))
+	F=int(input("Fitness"))
+	if (H_S+C_A++F)<=24:
+		weekend=lambda x : True if(x>=5) else False
+		return [datetime.date.today(), weekend(datetime.date.today().weekday()),H_S,C_A,F]
+	else:
+		print("Incorrect data ")
+		return {}
+
+def quality_Index():
+	''' takes input from the user for quality index '''
+	limiter=lambda x : 1 if (x<=1) else(10 if (x>=10) else x) #limits QOS and QOH to range 1-10
+	QOS=int(input("quality of sleep(1-10)"))
+	QOH=int(input("happiness (1-10)"))
+	co=input("Comment ")
+	return [limiter(QOS),limiter(QOH),co]
+
+def project():
+	'''takes input for project hours from user'''
+	 p_id = int(input("project"))
+	 p_name=input("Project name ")
+	 hs=int(input("hourse spent on this project"))
+	 return [p_id,p_name,hs]
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pandas as pd
+
 import insert_data as idt
 
-df=pd.DataFrame(columns=['date','Weekend','Amount of Sleep(in hours)','Quality of sleep(0-10)','classes attended ','Time devoted to self Learning','Time devoted to class work','mood','alchol/pot'])
+#test code
+di=idt.daily_routine()
+dq=idt.quality_Index()
+p=idt.project()
+print(di,dq,p)
 
-di=idt.inputw()
-df2=pd.DataFrame([di])
-df=df.append(df2)
-df.to_excel("routine.xlsx")
